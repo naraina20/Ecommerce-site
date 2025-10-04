87f2e1ed691009ca40949e2bb625941066b2f82e
Naraina <96872314+naraina20@users.noreply.github.com>
2025-10-04 23:35:00 +0530

    changes at cookie reset on logout

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -70,6 +70,8 @@ exports.userlogout = catchAsyncError(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: true,
+    sameSite: "none"
   });
 
   res.status(200).json({
